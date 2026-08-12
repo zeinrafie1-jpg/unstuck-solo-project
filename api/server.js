344a915c51080daef9a8d83a184c531b5758d73b
Zein Rafie <zeinrafie1@gmail.com>
2026-08-12 16:22:22 +0100

    Use FRONTEND_URL env variable for CORS origin

--- api/server.js
+++ api/server.js
@@ -11,7 +11,7 @@ connectDB();
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:5173', // frontend URL 
+  origin: process.env.FRONTEND_URL || 'http://localhost:5173', // frontend URL 
   credentials: true, // allow cookies to be sent 
 }));
 app.use(express.json());
